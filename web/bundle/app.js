b0fcb378ab8f9957ada7ccc44003ec03720da6e7
admin <pun.bxxx@gmail.com>
2020-08-05 15:30:02 +0700

    #pun //แก้ config line

--- web/bundle/app.js
+++ web/bundle/app.js
@@ -13,7 +13,8 @@ const UDH_PROFILE = "udh-profile";
 var config = {
   redirectUri: "https://www.udhconnect.info",
   liffId: "1654023325-EkWmY9PA",
-
+  ChannelAccessToken:"FWZ3P4fRrEXOmhyQtiQFp+TXeSSrkQwGdt3zvp1TezV9gYOruopsbo4YDBjoIKSoWzd/Yx/Ow/8xT0Elwvv6N+akUpPXtdMOdi5NN+t8BMHiVFWoDopJLEn0fUJSg0Rink0gBjXMSwcKIoI6FmoaQQdB04t89/1O/w1cDnyilFU=",
+  RichMenuId:"richmenu-349a649ee1b2e2f659ae2da8e24df4ef"
 };
 
 var udhApp = {
@@ -106,20 +107,13 @@ var udhApp = {
     //เปลี่ยนเมนู
      var profile = this.getProfileStorage();
      var userId = profile.userId;
-    //var userId = "Udeadbeefdeadbeefdeadbeefdeadbeef";
-    //var richMenuId = "richmenu-349a649ee1b2e2f659ae2da8e24df4ef";
-    var richMenuId = "richmenu-349a649ee1b2e2f659ae2da8e24df4ef";
     $.ajax({
       method: "POST",
-      url: `https://api.line.me/v2/bot/user/${userId}/richmenu/${richMenuId}`,
+      url: `https://api.line.me/v2/bot/user/${userId}/richmenu/${config.RichMenuId}`,
       dataType: "JSON",
       beforeSend: function(xhr) {
         //ไลน์พี่บอล
-        // xhr.setRequestHeader("Authorization", "Bearer FWZ3P4fRrEXOmhyQtiQFp+TXeSSrkQwGdt3zvp1TezV9gYOruopsbo4YDBjoIKSoWzd/Yx/Ow/8xT0Elwvv6N+akUpPXtdMOdi5NN+t8BMHiVFWoDopJLEn0fUJSg0Rink0gBjXMSwcKIoI6FmoaQQdB04t89/1O/w1cDnyilFU=" );
-        xhr.setRequestHeader(
-          "Authorization",
-          "Bearer uLF9THsOlQfvth3Y7bvLym0ZwPoEliKF7MszmJq4aymKwWJfYpknJ/zmWwOZsNzgrDXU0+Y7KGMrxCPi79NX1/g3iSeY5Mva1olEL4cwoJtDdznKV+7MjYP89tW6BO8/A//QjXTcoB6BdDt6ooFzB1GUYhWQfeY8sLGRXgo3xvw="
-        );
+        xhr.setRequestHeader("Authorization", "Bearer " + config.ChannelAccessToken);
       },
       error: function(jqXHR, textStatus, errorThrown) {
         Swal.fire({
