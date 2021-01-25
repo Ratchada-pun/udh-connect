fad62dcc3116876f8cf095bedddfceda0aec8684
admin <pun.bxxx@gmail.com>
2021-01-25 13:49:08 +0700

    #pun //แก้ไข hn

--- common/components/AppQuery.php
+++ common/components/AppQuery.php
@@ -680,7 +680,7 @@ class AppQuery
             ->orderBy('Appoint.appoint_date DESC');
         if ($profile['hn']) {
             $query->andWhere([
-                'Appoint.hn' =>  $profile['hn']
+                'Appoint.hn' =>  Util::sprintf($profile['hn'], 7)
             ]);
         }
         if ($profile['id_card']) {
